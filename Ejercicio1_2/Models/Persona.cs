e0ef12a30714f503669aeb2536b63ba8d02be072
oscar <oscardiazmaldonado93@gmail.com>
2023-02-24 23:37:08 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1_2/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1_2.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Edad { get; set; }
+
+        public string Correo { get;set; }
+    }
+}
